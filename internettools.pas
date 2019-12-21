5f2f0a98dd42b0d4779238c558230bb544dc79b1
Miguel A. Risco-Castillo <mriscoc@gmail.com>
2019-12-21 07:37:30 -0500

    Updated personal changes

--- internettools.pas
+++ internettools.pas
@@ -8,10 +8,11 @@ unit internettools;
 interface
 
 uses
-  bbutils, extendedhtmlparser, simpleinternet, internetaccess, simplehtmlparser, simplehtmltreeparser, simplexmlparser, xquery, 
-  synapseinternetaccess, w32internetaccess, simplexmltreeparserfpdom, xquery_json, mockinternetaccess, xquery__regex, xquery__parse, 
-  xquery_module_math, xquery__functions, multipagetemplate, xquery.internals.common, xquery.namespaces, 
-  xquery.internals.protectionbreakers, xquery.internals.lclexcerpt, LazarusPackageIntf;
+  bbutils, extendedhtmlparser, simpleinternet, internetaccess, 
+  simplehtmlparser, simplehtmltreeparser, simplexmlparser, xquery, 
+  synapseinternetaccess, w32internetaccess, simplexmltreeparserfpdom, 
+  xquery_json, mockinternetaccess, xquery__regex, xquery__parse, 
+  xquery_module_math, xquery__functions, multipagetemplate, LazarusPackageIntf;
 
 implementation
 
